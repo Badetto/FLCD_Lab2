baba8429fc4dd0080a7555ff16fca11750478001
Badu Catalin-Ioachim <catalinbaduca@gmail.com>
2023-12-03 17:46:27 +0200

    Follow function

--- Lab_Teams/Parser.cs
+++ Lab_Teams/Parser.cs
@@ -49,5 +49,45 @@ namespace Lab_Teams
 
             return firstSet;
         }
+
+        public HashSet<string> Follow(string nonTerminal)
+        {
+            HashSet<string> followSet = new HashSet<string>();
+
+            if (nonTerminal == grammar.startingSymbol)
+            {
+                followSet.Add("$");
+            }
+
+            foreach (var production in grammar.productions)
+            {
+                for (int i = 0; i < production.Value.Count; i++)
+                {
+                    var symbols = production.Value.ElementAt(i);
+                    if (symbols.Contains(nonTerminal))
+                    {
+                        int position = symbols.IndexOf(nonTerminal);
+
+                        if (position < symbols.Count - 1)
+                        {
+                            var nextSymbol = symbols[position + 1];
+                            var firstOfNextSymbol = First(nextSymbol);
+                            followSet.UnionWith(firstOfNextSymbol.Where(x => x != "ε"));
+
+                            if (firstOfNextSymbol.Contains("ε"))
+                            {
+                                followSet.UnionWith(Follow(production.Key[0]));
+                            }
+                        }
+                        else
+                        {
+                            followSet.UnionWith(Follow(production.Key[0]));
+                        }
+                    }
+                }
+            }
+
+            return followSet;
+        }
     }
 }
